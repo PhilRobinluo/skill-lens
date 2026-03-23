df111f95f4aaa6a385a0baf5202d584298b74eb1
Phil-Mini <32173131+PhilRobinluo@users.noreply.github.com>
2026-03-23 15:01:46 +0800

    auto: update preload.mjs

--- electron/preload.mjs
+++ electron/preload.mjs
@@ -1,5 +1,10 @@
 import { contextBridge, ipcRenderer } from "electron";
 
+// 给 HTML 标记 Electron 环境，CSS 可以直接用这个 class
+window.addEventListener("DOMContentLoaded", () => {
+  document.documentElement.classList.add("electron");
+});
+
 contextBridge.exposeInMainWorld("electronAPI", {
   isElectron: true,
   openMainWindow: () => ipcRenderer.invoke("open-main-window"),
